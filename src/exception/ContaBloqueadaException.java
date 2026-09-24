641ac79eb910c8892f9d648daaec179c8a8a2f3a
Allan <allangustavo2018@gmail.com>
2026-09-24 20:21:38 -0300

    feat: cria exceções customizadas

---
+++ src/exception/ContaBloqueadaException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ContaBloqueadaException extends Exception {
+
+    public ContaBloqueadaException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/exception/ValorInvalidoException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ValorInvalidoException extends Exception {
+
+    public ValorInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
